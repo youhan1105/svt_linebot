eebbf65c3bbec93c47016f41c9a85b3dbb92f7f5
youhan1105 <70013946+youhan1105@users.noreply.github.com>
2024-01-09 22:49:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import random
 
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+
 #Linebot設定
 channel_access_token = 'mCJ2+jdUUJZ7gvYlTbhHFcs9MPyXn16iV/67s376Fif/XG5a4Mo++0mkcwn2opdG5ExcAcgygV67cGfvBaMO4+sKIyjkuehgmIK1UsZX1CDTZ1FhFjREv4Nr9Mt0Hh6EJ8yDYxrI2stTMfvgDbDnxwdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(channel_access_token)
@@ -16,15 +20,6 @@ scope = ['https://www.googleapis.com/auth/spreadsheets',
 
 creditials = ServiceAccountCredentials.from_json_keyfile_name('gs_credentials.json', scope)
 client = gspread.authorize(creditials)
-
-#建立新的試算表後分享給自己
-#sheet = client.create("First sheet")
-#sheet.share('claire60320@gmail.com',perm_type='user',role='writer')
-
-#本地檔案寫入Googlesheet
-#sheet= client.open("First sheet").sheet1
-#df = pd.read_csv('goingsvt.csv')
-#sheet.update([df.columns.values.tolist()] + df.values.tolist())
 
 # 處理收到的訊息事件
 def handle_message(event):
