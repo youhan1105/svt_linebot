92a49a1bd621f4209a5b1f705c22fbe915560052
youhan1105 <70013946+youhan1105@users.noreply.github.com>
2024-01-29 06:54:08 +0800

    Update message_handler.py

--- message_handler.py
+++ message_handler.py
@@ -1,8 +1,5 @@
-from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage,TextSendMessage, ImageSendMessage, QuickReply, QuickReplyButton, MessageAction
 from linebot.exceptions import InvalidSignatureError
-from oauth2client.service_account import ServiceAccountCredentials
 
 import gspread
 import os
