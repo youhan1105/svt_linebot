07101a3904b3cc7b77f8f1dca2fd3cf5bf3e6652
youhan1105 <70013946+youhan1105@users.noreply.github.com>
2024-02-20 15:59:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -69,13 +69,13 @@ def handle_message(event):
 
     if user_data is None:
         user_image_index = {}
-        ref.child(user_id).set({'user_image_index': None})
+        ref.child(user_id).set({'user_image_index': user_image_index})
     else:
         user_image_index = user_data.get('user_image_index')
         
         if user_image_index is None:
             user_image_index = {}
-            
+
     if user_input == str('抽'):
         image_urls = []
         random_row = random.choice(data)  
@@ -97,26 +97,26 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, image_messages)
 
     elif user_input == str('取得編號'):
-        if user_id in user_image_index:
+        if user_image_index is not None and user_id in user_image_index:
             current_row_index = user_image_index[user_id]
-            if current_row_index is not None:
-                if current_row_index < len(data):
-                    current_row = data[current_row_index]
-                    image_number = current_row.get('編號')
-                    image_name = current_row.get('中字')
+            if current_row_index is not None and current_row_index < len(data):
+                current_row_index < len(data):
+                current_row = data[current_row_index]
+                image_number = current_row.get('編號')
+                image_name = current_row.get('中字')
 
-                    quick_reply_items = [
-                        QuickReplyButton(action=MessageAction(label='上一張', text='上一張')),
-                        QuickReplyButton(action=MessageAction(label='下一張', text='下一張')),
-                        QuickReplyButton(action=MessageAction(label='抽', text='抽'))
-                    ]
-                    quick_reply = QuickReply(items=quick_reply_items)
+                quick_reply_items = [
+                    QuickReplyButton(action=MessageAction(label='上一張', text='上一張')),
+                    QuickReplyButton(action=MessageAction(label='下一張', text='下一張')),
+                    QuickReplyButton(action=MessageAction(label='抽', text='抽'))
+                ]
+                quick_reply = QuickReply(items=quick_reply_items)
 
-                    # 建立回覆訊息，包含 Quick Reply 按鈕
-                    text_message = TextSendMessage(text=f"圖片編號為：\n【{image_number}】{image_name}", quick_reply=quick_reply)
+                # 建立回覆訊息，包含 Quick Reply 按鈕
+                text_message = TextSendMessage(text=f"圖片編號為：\n【{image_number}】{image_name}", quick_reply=quick_reply)
 
         
-                    line_bot_api.reply_message(event.reply_token, text_message)
+                line_bot_api.reply_message(event.reply_token, text_message)
         else:
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text="請先抽圖片"))
 
