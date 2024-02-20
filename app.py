196dc5833bf1519b859cb4b96cf2f7398b816496
youhan1105 <70013946+youhan1105@users.noreply.github.com>
2024-02-21 03:06:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,30 +71,28 @@ def handle_message(event):
     #region #Firebase資料
     ref = db.reference('/')
     fire_data = ref.get()
+
     if fire_data is None:
         fire_data = {}
-    user_image_index = fire_data.get('user_image_index', {})
+
+    user_data = fire_data.get(user_id, {})
+    user_image_index = user_data.get('user_image_index', {})
     current_row_index = user_image_index.get(user_id)
+
     user_data = fire_data.get(user_id)
     print('current_row_index-0:',current_row_index)
-    #endregion
 
+    #endregion
+    # 如果 user_id 不在 user_image_index 中，則進行初始化
     if user_id not in user_image_index:
         user_image_index[user_id] = None
-
-        if user_data is None:
-            user_image_index = {}
-            user_data = {'user_image_index': user_image_index}
-            ref.child(user_id).set(user_data)
-
-    else:
-        user_image_index = user_data.get('user_image_index', {})
         print('current_row_index-1:',current_row_index)
 
-        if user_image_index is None or not isinstance(user_image_index, dict):
-            user_image_index = {}
+        #如果 user_data 不存在，則初始化為空字典並設置到 Firebase 中
+        if not user_data:
+            user_data = {'user_image_index': user_image_index}
+            ref.child(user_id).set(user_data)
             print('current_row_index-2:',current_row_index)
-
 
     emoji_mapping = {
         emoji.emojize("🍒"): "1",
